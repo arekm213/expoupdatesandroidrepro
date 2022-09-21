9f6de06a8dd38e7e7779e322c8b8f25f60bc7c71
arekm213 <Arqm213@gmail.com>
2022-09-21 18:39:37 +0200

    Set up expo, expo-updates and release signing

--- android/app/src/main/java/com/expoupdatesandroidrepro/MainActivity.java
+++ android/app/src/main/java/com/expoupdatesandroidrepro/MainActivity.java
@@ -3,6 +3,7 @@ package com.expoupdatesandroidrepro;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import expo.modules.ReactActivityDelegateWrapper;
 
 public class MainActivity extends ReactActivity {
 
@@ -22,7 +23,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new MainActivityDelegate(this, getMainComponentName());
+    return new ReactActivityDelegateWrapper(this, new MainActivityDelegate(this, getMainComponentName()));
   }
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
